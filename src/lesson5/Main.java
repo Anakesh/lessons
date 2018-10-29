d3398b6107cb12aae94551bf8ffe8ede5c6ca290
java-ifmo <37559128+java-ifmo@users.noreply.github.com>
2018-10-29 18:39:27 +0300

    Update Main.java

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -7,8 +7,8 @@ import lesson5.battleunit.Spearman;
 public class Main {
     public static void main(String[] args) {
         Knight knight = new Knight(100, 20);
-        Knight knight1 = new Knight(100, 20);
+        Spearman spearman = new Spearman(60, 70);
 
-        knight.attack(knight1);
+        knight.attack(spearman);
     }
 }
